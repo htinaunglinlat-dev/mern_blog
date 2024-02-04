3872acd9ac7030ce2b574bf10815cc93dcdc445b
Htin Aung Lin Lat <htinaunglinlat1303@gmail.com>
2024-02-04 21:35:10 +0630

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+   .connect(process.env.MONGO)
+   .then(() => {
+      console.log("mongodb is connected")
+   })
+   .catch(error => {
+      console.log("Connection error: " + error)
+   })
+
 
 const app = express();
 
