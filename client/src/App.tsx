88e12628bf764abb84d1b0e16e7608f798c3c794
Htin Aung Lin Lat <htinaunglinlat1303@gmail.com>
2024-02-04 17:09:11 +0630

    create pages and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Dashboard from "./pages/Dashboard"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Projects from "./pages/Projects"
+
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/projects " element={<Projects />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
