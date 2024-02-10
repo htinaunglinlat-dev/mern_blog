8c8e4ac79ea26819d8e0ae87899dac9dc94c751d
Htin Aung Lin Lat <htinaunglinlat1303@gmail.com>
2024-02-10 21:26:42 +0630

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
    const { username, email, password } = req.body;
@@ -23,4 +24,28 @@ export const signup = async (req, res, next) => {
    } catch (error) {
       return next(error);
    }
+}
+
+export const signin = async (req, res, next) => {
+   const { email, password } = req.body;
+   if(!email || !password || email === "" || password === "") 
+      return next(errorHandler(400, "All fields are required!"));
+   try{
+      const validUser = await User.findOne({ email: email });
+      if(!validUser)
+         return next(errorHandler(404, "User not found!"));
+      const validPassword = bcrypt.compareSync(password, validUser.password);
+      if(!validPassword)
+         return next(errorHandler(400, "Incorrect password!"));
+      const token = jwt.sign(
+         {userId: validUser._id},
+         process.env.JWT_SECRET_KEY,
+      )
+      const { password: pass, ...rest } = validUser._doc;
+      
+      return res.status(200).cookie("access_token", token, {httpOnly: true}).json(rest)
+   } catch(error) {
+      return next(error)
+   }
+
 }
--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ mongoose
    })
 
 app.listen(3000, () => {
-   console.log('Example app listening on port 3000!');
+   console.log('Sever is listening on port 3000!');
 });
 
 app.use(express.json());
